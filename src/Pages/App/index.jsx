7618c254c25e0c642229c07583810ca2441dc408
Law <victorestrada2006@gmail.com>
2023-06-03 17:40:23 +0200

    Enrutamiento

---
+++ src/Pages/App/index.jsx
@@ -0,0 +1,32 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom'
+import './App.css'
+import { Home } from '../Home'
+import { MyAccount } from '../MyAccount'
+import { MyOrder } from '../MyOrder'
+import { MyOrders} from '../MyOrders'
+import { NotFound } from '../NotFound'
+import { SignIn } from '../SignIn'
+
+
+const AppRoutes = ()=>{
+  let routes = useRoutes([
+    { path: '/', element: <Home/> },
+    { path: '/MyAccount', element: <MyAccount/> },
+    { path: '/MyOrders', element: <MyOrders/> },
+    { path: '/MyOrder', element: <MyOrder/> },
+    { path: '/*', element: <NotFound/> },
+    { path: '/SignIn', element: <SignIn/> }
+  ])
+
+  return routes
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppRoutes/>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/Pages/NotFound/index.jsx
@@ -0,0 +1,10 @@
+
+function NotFound(){
+    return(
+        <div className="bg-red-100">
+            404
+        </div>
+    )
+}
+
+export {NotFound}
